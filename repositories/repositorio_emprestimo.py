c484c9662a2ba46b5f332a13dadc61ae19b9485d
Cauã Negrão <cauamn18@gmail.com>
2026-05-06 22:05:26 -0300

    Create repositorio_emprestimo.py

---
+++ repositories/repositorio_emprestimo.py
@@ -0,0 +1,12 @@
+class RepositorioEmprestimo:
+    def buscar_por_id(self, id: int):
+        """Stub de Acesso a Dados"""
+        ...
+
+    def salvar(self, objeto):
+        """Stub de Acesso a Dados"""
+        ...
+
+    def listar_todos(self):
+        """Stub de Acesso a Dados"""
+        ...
