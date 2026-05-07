3241118469a435ce815521faed7b3cabfcbee784
Cauã Negrão <cauamn18@gmail.com>
2026-05-06 22:03:42 -0300

    Create notificador.py

---
+++ services/notificador.py
@@ -0,0 +1,8 @@
+class Notificador:
+    def enviar_confirmacao(self, email: str):
+        """Stub de Notificação"""
+        ...
+
+    def enviar_alerta_atraso(self, email: str):
+        """Stub de Notificação"""
+        ...
