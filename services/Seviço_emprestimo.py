eff722e42e05bdbe50c74f1857d3cddd7c1eb46b
Cauã Negrão <cauamn18@gmail.com>
2026-05-06 22:02:43 -0300

    Create Seviço_emprestimo.py

---
+++ services/Seviço_emprestimo.py
@@ -0,0 +1,12 @@
+class ServicoEmprestimo:
+    def registrar(self, equip_id: int, nome: str, email: str, dias: int):
+        """Stub do UC01"""
+        ...
+
+    def devolver(self, emprestimo_id: int):
+        """Stub do UC02"""
+        ...
+
+    def listar_atrasados(self):
+        """Stub do UC03"""
+        ...
