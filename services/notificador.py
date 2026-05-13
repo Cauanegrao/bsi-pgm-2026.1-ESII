b4b12d275ada4050c767676aa06e86a9f0c6fbeb
Cauã Negrão <cauamn18@gmail.com>
2026-05-12 23:25:17 -0300

    Update notificador.py

--- services/notificador.py
+++ services/notificador.py
@@ -2,19 +2,9 @@
 
 class Notificador:
     def enviar_confirmacao(self, email: str, item: str):
-        """Simula o envio de um e-mail de sucesso após o registro."""
-        print("-" * 30)
-        print(f"[SIMULAÇÃO DE EMAIL]")
-        print(f"Para: {email}")
-        print(f"Assunto: Confirmação de Empréstimo")
-        print(f"Mensagem: Olá! O item '{item}' foi reservado com sucesso.")
-        print("-" * 30)
+        print(f"\n[EMAIL] Enviado para: {email}")
+        print(f"Mensagem: O item '{item}' foi reservado com sucesso.")
 
     def enviar_alerta_atraso(self, email: str, multa: float):
-        """Simula o envio de um alerta quando há atraso na devolução."""
-        print("-" * 30)
-        print(f"[SIMULAÇÃO DE EMAIL]")
-        print(f"Para: {email}")
-        print(f"Assunto: Alerta de Atraso!")
-        print(f"Mensagem: Atenção! Seu empréstimo está atrasado. Multa atual: R$ {multa:.2f}")
-        print("-" * 30)
+        print(f"\n[EMAIL] Alerta para: {email}")
+        print(f"Atenção: Devolução em atraso. Multa: R$ {multa:.2f}")
