cd3cc30443e7c76a6469e793d8a9742a2ace5c2e
Cauã Negrão <cauamn18@gmail.com>
2026-05-12 22:45:21 -0300

    Update notificador.py

--- services/notificador.py
+++ services/notificador.py
@@ -1,3 +1,10 @@
+# Notificador: Processar o envio de comunicações externas.
+
+class Notificador:
+    def enviar_confirmacao(self, email: str, item: str):
+        print(f"[SIMULAÇÃO EMAIL] Para: {email} | Item: {item} reservado com sucesso.")
+        
+
 class Notificador:
     def enviar_confirmacao(self, email: str):
         """Stub de Notificação"""
