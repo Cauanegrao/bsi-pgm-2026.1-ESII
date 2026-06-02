2084f4df8142a139496aaf4a61493701bdda1420
Cauã Negrão <cauamn18@gmail.com>
2026-06-02 16:21:50 -0300

    refactor: introduz interfaces abc.ABC para Repositorio e Notificador

--- services/notificador.py
+++ services/notificador.py
@@ -1,10 +1,19 @@
 # Notificador: Processar o envio de comunicações externas.
+from serviços.interfaces import INotificador
+from datetime import date
 
-class Notificador:
-    def enviar_confirmacao(self, email: str, item: str):
+class Notificador(INotificador):
+    def notificar_emprestimo(self, email: str, data_devolucao: date) -> None:
+        # Usando a lógica do seu método enviar_confirmacao
         print(f"\n[EMAIL] Enviado para: {email}")
-        print(f"Mensagem: O item '{item}' foi reservado com sucesso.")
+        print(f"Mensagem: Empréstimo registrado com sucesso. Data de devolução: {data_devolucao}")
 
-    def enviar_alerta_atraso(self, email: str, multa: float):
+    def notificar_devolucao(self, email: str, multa: float) -> None:
+        # Usando a lógica do seu método enviar_alerta_atraso
         print(f"\n[EMAIL] Alerta para: {email}")
         print(f"Atenção: Devolução em atraso. Multa: R$ {multa:.2f}")
+
+    def notificar_atraso(self, email: str) -> None:
+        # Método obrigatório da interface para avisar sobre pendências
+        print(f"\n[EMAIL] Alerta de pendência enviado para: {email}")
+        print("Atenção: Você possui um equipamento em atraso!")
