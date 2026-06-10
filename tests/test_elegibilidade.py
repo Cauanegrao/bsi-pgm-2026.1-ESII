6356e384aa5e0ee2f62713132ccef281b24d9109
Cauã Negrão <cauamn18@gmail.com>
2026-06-09 22:19:02 -0300

    feat: bloqueia aluno com excesso de emprestimos (autonoma ciclo 2)

--- tests/test_elegibilidade.py
+++ tests/test_elegibilidade.py
@@ -4,3 +4,7 @@ from elegibilidade import verificar_elegibilidade_aluno
 def test_aluno_sem_emprestimos_ativos_esta_elegivel():
     resultado = verificar_elegibilidade_aluno(qtd_emprestimos_ativos=0, email="caua@ufra.edu.br")
     assert resultado is True
+
+def test_aluno_com_tres_ou_mais_emprestimos_fica_bloqueado():
+    resultado = verificar_elegibilidade_aluno(qtd_emprestimos_ativos=3, email="caua@ufra.edu.br")
+    assert resultado is False
