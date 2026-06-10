ee2310dfa2092691473c4ff9c66e14e8228648df
Cauã Negrão <cauamn18@gmail.com>
2026-06-09 21:57:47 -0300

    feat: cobra dias excedentes apos carencia (ciclo 2)

--- multa.py
+++ multa.py
@@ -1,2 +1,6 @@
 def calcular_multa_com_carencia(dias_atraso, valor_dia, carencia=2):
-    return 0.0 
+    if dias_atraso <= carencia:
+        return 0.0
+    
+    dias_cobrados = dias_atraso - carencia
+    return float(dias_cobrados * valor_dia)
