30a1597511fb793d69f3e0fec972c509caa59271
Cauã Negrão <cauamn18@gmail.com>
2026-06-09 21:53:29 -0300

    feat: multa zero quando sem atraso (ciclo 1)

---
+++ tests/test_multa.py
@@ -0,0 +1,6 @@
+import pytest
+from multa import calcular_multa_com_carencia
+
+def test_multa_zero_quando_sem_atraso():
+    multa = calcular_multa_com_carencia(dias_atraso=0, valor_dia=10.0)
+    assert multa == 0.0
