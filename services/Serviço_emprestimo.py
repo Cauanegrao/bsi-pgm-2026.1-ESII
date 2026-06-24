6d5544b680e32bdb49591ad43ea00c8169c30ade
Cauã Negrão <cauamn18@gmail.com>
2026-06-23 21:58:03 -0300

    Update Serviço_emprestimo.py

--- services/Serviço_emprestimo.py
+++ services/Serviço_emprestimo.py
@@ -1,10 +1,11 @@
 # ServicoEmprestimo: Executar as regras de negócio de locação.
 from datetime import date, timedelta
+from services.observer import Subject 
 
-class ServicoEmprestimo:
-    def __init__(self, repositorio_emprestimo, notificador):
+class ServicoEmprestimo(Subject): 
+    def __init__(self, repositorio_emprestimo): 
+        super().__init__() 
         self.repo = repositorio_emprestimo
-        self.notificador = notificador
 
     def registrar(self, equip_id: int, nome: str, email: str, dias: int):
         equip = self.repo.buscar_por_id(equip_id)
@@ -23,7 +24,13 @@ class ServicoEmprestimo:
             }
             
             self.repo.salvar(novo_emprestimo)
-            self.notificador.enviar_confirmacao(email, equip["nome"])
+            
+            self.notificar({
+                "tipo": "emprestimo", 
+                "email": email, 
+                "data": data_dev
+            })
+            
             return True
         return False
 
@@ -32,10 +39,27 @@ class ServicoEmprestimo:
             if emp["id"] == emprestimo_id and not emp["devolvido"]:
                 emp["devolvido"] = True
                 equip = self.repo.buscar_por_id(emp["equip_id"])
-                if equip: equip["disponivel"] = True
+                if equip: 
+                    equip["disponivel"] = True
+
+                multa = 0.0  
+                self.notificar({
+                    "tipo": "devolucao", 
+                    "email": emp["email_aluno"], 
+                    "multa": multa
+                })
+                
                 return True
         return False
 
     def listar_atrasados(self):
         hoje = date.today()
-        return [e for e in self.repo.listar_todos() if e["data_devolucao"] < hoje and not e["devolvido"]]
+        atrasados = [e for e in self.repo.listar_todos() if e["data_devolucao"] < hoje and not e["devolvido"]]
+        
+        for emp in atrasados:
+            self.notificar({
+                "tipo": "atraso", 
+                "email": emp["email_aluno"]
+            })
+            
+        return atrasados
